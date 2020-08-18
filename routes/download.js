c0a9230920fadb2c771c78668a22c57d9090491c
Rek <altrekdv7@gmail.com>
2020-08-18 17:40:52 +0000

    update

--- routes/download.js
+++ routes/download.js
@@ -3,11 +3,8 @@ const router = express.Router();
 const http = require('http');
 
 router.get('/:id.mp3', function(req, res, next) {
-	const fs = require('fs');
-
 	var path = `./temps/${req.params.id}.mp3`
 	res.download(path);
-	setTimeout(fs.unlink(path), 15000);
 });
 
 module.exports = router;
--- routes/getmp3.js
+++ routes/getmp3.js
@@ -9,7 +9,7 @@ router.get('/:text', function(req, res, next) {
 	//Functions
 	function random(length) {
 	var result           = '';
-	var characters       = '0123456789';
+	var characters       = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 	var charactersLength = characters.length;
 	for ( var i = 0; i < length; i++ ) {
 		result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -17,7 +17,7 @@ router.get('/:text', function(req, res, next) {
 	return result;
 	}
 
-	var id = random(64)
+	var id = random(8)
 	var path = `./temps/${id}.mp3`
 	const file = fs.createWriteStream(path);
 	const request = http.get(`http://translate.google.com/translate_tts?ie=UTF-8&tl=vi&client=tw-ob&q=${req.params.text}`, function(response) {
