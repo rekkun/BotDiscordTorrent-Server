27d7b1f3c83582dbdd9ac68bfe75c012f080df8c
Rek <altrekdv7@gmail.com>
2020-08-18 17:18:57 +0000

    update

--- routes/download.js
+++ routes/download.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const http = require('http');
 
-router.get('/:id', function(req, res, next) {
+router.get('/:id.mp3', function(req, res, next) {
 	const fs = require('fs');
 
 	var path = `./temps/${req.params.id}.mp3`
